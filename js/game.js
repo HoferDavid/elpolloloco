b61526ccc57a9cb951b12102f8681f4b517f39dd
David Hofer <chassis.poppets0x@icloud.com>
2024-08-25 17:51:38 +0200

    Restart Bug Fix

--- js/game.js
+++ js/game.js
@@ -5,26 +5,32 @@ let audioMuted = sessionStorage.getItem("audioMuted") === "true";
 
 
 /**
- * This function handles showing the initial game screen and calls the `resetGame` method to initialize the game.
+ * Starts the game by hiding the start game screen and calling the `resetGame` function to initialize the game state.
+ * This function also toggles the visibility of the fullscreen button based on the screen size.
  */
 function startGame() {
-  handleDisplayStyle("startGameScreen");
+  document.getElementById("startGameScreen").style.display = "none";
   resetGame();
+  toggleFullscreenBtn();
 }
 
 
 /**
- * This function handles showing the restart game screen and calls the `resetGame` method to reinitialize the game.
+ * Restarts the game by hiding the restart game screen, removing the 'positionAbsolute' class from the canvas,
+ * and calling the `resetGame` function to reinitialize the game state.
+ * This function updates the canvas display and ensures that the game is reset to its initial state.
  */
 function reStartGame() {
-  handleDisplayStyle("restartGameScreen");
+  document.getElementById("canvas").classList.remove('positionAbsolute');
+  document.getElementById("restartGameScreen").style.display = "none";
   resetGame();
 }
 
 
 /**
- * This function configures the canvas and controls overlay, initializes the game world, and manages audio settings
- * based on the user's preferences stored in sessionStorage.
+ * This function displays the canvas and controls overlay, initializes the level, sets up the game world, 
+ * and manages audio settings based on the user's preferences stored in sessionStorage. 
+ * Additionally, it toggles the visibility of the fullscreen button based on the screen size.
  */
 function resetGame() {
   document.getElementById("canvas").style.display = "flex";
@@ -33,30 +39,16 @@ function resetGame() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
   audioMuted = sessionStorage.getItem("audioMuted") === "true";
-  if (audioMuted) { world.audio.muteAudio(true) } 
+  if (audioMuted) { world.audio.muteAudio(true) }
   else { world.audio.soundtrack.play() }
+  toggleFullscreenBtn();
 }
 
 
 /**
- * This function shows or hides elements based on the screen width and optionally toggles fullscreen mode.
- *
- * @param {string} id - The ID of the element to show or hide.
- */
-function handleDisplayStyle(id) {
-  let { width, height } = getScreenSize();
-  if (width <= 932) {
-    document.getElementById(id).style.display = "flex";
-    toggleFullscreen();
-  } else {
-    document.getElementById(id).style.display = "none";
-  }
-}
-
-
-/**
- * This function displays the result screen (win or game over), plays the corresponding sound, 
- * and hides the controls overlay and start/restart game screens. It also manages fullscreen mode.
+ * This function hides the controls overlay, sets the canvas to an absolute position, displays the result screen,
+ * and plays the result sound. After the sound duration, it hides the result screen and shows the restart game screen.
+ * It also exits fullscreen mode if it is currently active.
  *
  * @param {string} result - The result of the game, either "winScreen" or "gameOverScreen".
  */
@@ -65,10 +57,9 @@ function gameEnd(result) {
   clearAllIntervals();
   clearAllAudioIntervals();
   document.getElementById("controlsOverlay").style.display = "none";
+  document.getElementById("canvas").classList.add('positionAbsolute');
   document.getElementById(result).style.display = "flex";
   sound.play();
-  document.getElementById("restartGameScreen").style.display = "none";
-  document.getElementById("startGameScreen").style.display = "none";
   setTimeout(() => {
     document.getElementById(result).style.display = "none";
     toggleClasses("canvas", "restartGameScreen");
@@ -82,12 +73,12 @@ function gameEnd(result) {
  * stops all audio, and shows the restart game screen.
  */
 function backtoMenu() {
-  document.getElementById("startGameScreen").style.display = "none";
-  if (document.fullscreenElement) { document.exitFullscreen() }
   clearAllIntervals();
   clearAllAudioIntervals();
-  toggleClasses("canvas", "restartGameScreen");
+  if (document.fullscreenElement) { document.exitFullscreen() }
+  document.getElementById("canvas").style.display = "none";
   document.getElementById("controlsOverlay").style.display = "none";
+  document.getElementById("startGameScreen").style.display = "flex";
 }
 
 
@@ -111,7 +102,7 @@ function clearAllAudioIntervals() {
 
 /**
  * This function requests fullscreen mode if not already in fullscreen, or exits fullscreen mode if active.
- * It also updates the fullscreen button icon and calls `backtoMenu` if exiting fullscreen.
+ * It also updates the fullscreen button icon
  */
 function toggleFullscreen() {
   let element = document.getElementById("game");
@@ -121,8 +112,18 @@ function toggleFullscreen() {
   } else {
     document.exitFullscreen();
     document.getElementById("fullscreenBtn").src = "assets/img/icons/fullscreenOpen.svg";
-    backtoMenu();
   }
+}
+
+
+/**
+ * This function hides the fullscreen button if the screen width is less than or equal to 932 pixels,
+ * and shows it otherwise.
+ */
+function toggleFullscreenBtn() {
+  let { width, height } = getScreenSize();
+  if (width <= 932) { document.getElementById("fullscreenBtn").style.display = "none" } 
+  else { document.getElementById("fullscreenBtn").style.display = "flex" }
 }
 
 
